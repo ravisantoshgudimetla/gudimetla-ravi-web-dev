3c44619fbad11ac080733360f0f307efdca713c3
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2016-06-08 09:41:05 -0400

    Project changes

--- public/project/views/user/image-search.controller.client.js
+++ public/project/views/user/image-search.controller.client.js
@@ -7,11 +7,12 @@
         var vm = this;
         vm.searchPhotos = searchPhotos;
         //vm.selectPhoto = selectPhoto;
-
+        var uid = $routeParams["uid"];
         
 
         function searchPhotos(imageurl) {
-            url = "/project/user/:userId/search_images?imageurl=" + imageurl ;
+            url = "/project/user/get_images?imageurl=" + imageurl ;
+            console.log(url);
             vm.users = $http.get(url)
 
         }
